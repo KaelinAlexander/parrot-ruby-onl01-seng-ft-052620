b8c0c58fdb83f39f4e3f76b5fc4f4134c025f2ca
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-04 18:20:52 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,12 @@
 def parrot(sound="Squawk!")
+<<<<<<< HEAD
     puts sound
     sound
 end
 parrot
+=======
+    puts "Pretty bird!"
+    'Squawk!'
+    return 'Squawk!'
+end
+>>>>>>> 779a56f3c018450d24e57f6c8b5ca9f6551a5ab9
